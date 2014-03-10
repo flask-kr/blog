b7cdc3fdd447cf0f9f110637a877bddf3f132957
jacks0612 <jacks0612@github.com>
2014-03-10 17:10:40 +0900

    Edit home CSS & Add db_create def

--- web/__init__.py
+++ web/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
-
+from flask.ext.sqlalchemy import SQLAlchemy
+   
 def create_app():
     """creates Flask instance"""
     app = Flask(__name__)
 
     return app
+
+def create_db():
+    db =  SQLAlchemy()
+    return db
--- web/home/__init__.py
+++ web/home/__init__.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+from gethtml.extractimgs import extract_images
 
-home = Blueprint('home', __name__, template_folder='templates')
+module = Blueprint('home', __name__, template_folder='templates')
 
-@home.route('/home')
+@module.route('/home')
 def index():
     try:
-        print "here11"
         return render_template('home/index.html')
     except TemplateNotFound:
         abort(404)
